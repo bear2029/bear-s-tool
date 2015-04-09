55e6e5d0f199d33bd17ef0bfa7cbd6d767d86c15
bear <bear2029@gmail.com>
2015-04-09 02:57:02 -0700

    added list url validator

--- public/js/crawler.js
+++ public/js/crawler.js
@@ -4,33 +4,45 @@ var Editor = React.createClass({
 	mixins: [React.addons.LinkedStateMixin],
 	getInitialState:function(){
 		return {
-			sampleListUrl: 'http://www.piaotian.net/html/6/6788/'
+			sampleListUrl: '',
+			isListUrlValid: false
 		};
-	},
-	onReset: function(){
-		this.state.sampleListUrl = "http://www.piaotian.net/html/6/6788/"
-		console.log(this.state)
 	},
 	onSubmit: function(e)
 	{
 		e.preventDefault();
 	},
-	onAnyElementChanged: function(e)
+	onListUrlMatchChanged: function(e)
 	{
-		console.log('change',e.target,this.state);
+		var regexString = $('input[name=list-url-regex]',React.findDOMNode(this)).val()
+		var url = $('input[name=list-url]',React.findDOMNode(this)).val()
+		var urlDiv = $('input[name=list-url]',React.findDOMNode(this)).parent()
+		var isValid = false;
+		if(url && regexString){
+			var regex = new RegExp(regexString);
+			isValid = url.match(regex);
+		}
+		this.setState({isListUrlValid: isValid});
+		if(isValid){
+			urlDiv.addClass('valid');
+			urlDiv.removeClass('invalid');
+		}else{
+			urlDiv.removeClass('valid');
+			urlDiv.addClass('invalid');
+		}
 	},
 	render: function() {
 		return (
 		<form class="form-horizontal" onSubmit={this.onSubmit}>
-			<div className="form-group">
-				<input  onKeyUp={this.onAnyElementChanged} type="url" className="form-control" placeholder="list URL for test" valueLink={this.linkState('sampleListUrl')}/>
+			<div className="form-group list-url">
+				<input name="list-url" onKeyUp={this.onListUrlMatchChanged} type="url" className="form-control" placeholder="list URL for test" valueLink={this.linkState('sampleListUrl')}/>
+				<div className="validation-icon"><span className="glyphicon glyphicon-ok" aria-hidden="true"></span></div>
 			</div>
 			<div className="form-group">
-				<input type="text" onKeyUp={this.onRegexChange} className="form-control" name="list-url-regex" placeholder="list URL regex"/>
+				<input type="text" onKeyUp={this.onListUrlMatchChanged} className="form-control" name="list-url-regex" placeholder="list URL regex"/>
 			</div>
 			<div className="form-group">
 				<button type="submit" className="btn btn-default" >Submit</button>
-				<button type="reset2" className="btn btn-default" onClick={this.onReset}>Reset</button>
 			</div>
 		</form>
 		);
