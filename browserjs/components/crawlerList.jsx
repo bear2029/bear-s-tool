76f35638f78dddd997567d8d96c3c6f3caf79558
bear2029 <bear2029@gmail.com>
2015-09-29 01:27:58 -0700

    fix login and crawler

--- browserjs/components/crawlerList.jsx
+++ browserjs/components/crawlerList.jsx
@@ -1,3 +1,4 @@
+var $ = require('jquery');
 var React = require('react');
 var Reflux = require('reflux');
 var CrawlerEditor = require('./crawlerEditor.jsx')
@@ -18,6 +19,7 @@ var CrawlerList = React.createClass(
 	},
 	onAddNew: function()
 	{
+		window.scroll(0,0);
 		CrawlerActions.add();
 		//var data = {'_source': {}}
 		//React.render(<CrawlerEditor data={data} onSubmitComplete={this.onSubmitComplete} />, document.getElementById('crawler-editor'));
@@ -28,7 +30,7 @@ var CrawlerList = React.createClass(
 		<div>
 			<div id="list">
 				{this.state.crawlers.map(function(item,i){
-					return <CrawlerItem key={i} data={item}/>;
+					return <CrawlerItem key={i} id={item.id} siteName={item.siteName} remoteUrl={item.collectionUrl}/>;
 				})}
 			</div>
 			<button className="btn btn-primary" onClick={this.onAddNew}>Add New &nbsp;<span className="glyphicon glyphicon-plus"></span></button>
